fc70088d3daf4fc2aa77e6eb242e70d06b973b07
Thomas Lindner <tclindner@gmail.com>
2016-05-22 13:35:59 -0500

    Add new type rules

--- tests/unit/rules/peerDependencies-typeTest.js
+++ tests/unit/rules/peerDependencies-typeTest.js
@@ -2,19 +2,19 @@
 
 const should = require('should');
 const requireHelper = require('../../require_helper');
-const lint = requireHelper('rules/man-type').lint;
+const lint = requireHelper('rules/peerDependencies-type').lint;
 
-describe('man-type Unit Tests', function() {
+describe('peerDependencies-type Unit Tests', function() {
   context('when package.json has node with incorrect type', function() {
     it('LintIssue object should be returned', function() {
       const packageJsonData = {
-        man: 'man'
+        peerDependencies: 'peerDependencies'
       };
-      const response = lint(packageJsonData);
+      const response = lint(packageJsonData, 'error');
 
-      response.lintId.should.equal('man-type');
+      response.lintId.should.equal('peerDependencies-type');
       response.lintType.should.equal('error');
-      response.node.should.equal('man');
+      response.node.should.equal('peerDependencies');
       response.lintMessage.should.equal('Type should be an Object');
     });
   });
@@ -22,7 +22,7 @@ describe('man-type Unit Tests', function() {
   context('when package.json does not have node', function() {
     it('true should be returned', function() {
       const packageJsonData = {};
-      const response = lint(packageJsonData);
+      const response = lint(packageJsonData, 'error');
 
       response.should.be.true();
     });
