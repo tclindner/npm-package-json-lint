46b9b6a9b07492f3593e2ea90c0b35414e59d6ce
tclindner <tclindner@users.noreply.github.com>
2016-10-15 10:51:24 -0500

    Add new valid-values-license rule (#21)
    Closes #19
    
    New rule: valid-values-license

---
+++ src/rules/valid-values-license.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const LintIssue = require('./../LintIssue');
+const isValidValue = require('./../validators/valid-values').isValidValue;
+const lintId = 'valid-values-license';
+const nodeName = 'license';
+const message = 'Invalid value for license';
+const ruleType = 'valid-values';
+
+/**
+ * Lints package.json file to check for valid values in the license field
+ *
+ * @param  {Object}   packageJsonData   Valid package.json object
+ * @param  {String}   lintType          'error' or 'warning'
+ * @param  {Array}    validValues       An array of valid values
+ * @return {Object|Boolean}             LintIssue object if invalid. True if valid
+ */
+const lint = function(packageJsonData, lintType, validValues) {
+  if (!isValidValue(packageJsonData, nodeName, validValues)) {
+    return new LintIssue(lintId, lintType, nodeName, message);
+  }
+
+  return true;
+};
+
+module.exports.lint = lint;
+module.exports.ruleType = ruleType;
