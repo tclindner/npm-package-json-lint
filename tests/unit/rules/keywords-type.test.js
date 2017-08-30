ef10bba6467df47663a243141cce9baa0b89e44d
Thomas Lindner <tclindner@gmail.com>
2017-08-29 21:28:10 -0500

    Update keywords rule to have a ruleType of standard.
    Also fix unit tests so it imports the current source file!

--- tests/unit/rules/keywords-type.test.js
+++ tests/unit/rules/keywords-type.test.js
@@ -1,21 +1,27 @@
 'use strict';
 
 const chai = require('chai');
-const lint = require('./../../../src/rules/files-type').lint;
+const {lint, ruleType} = require('./../../../src/rules/keywords-type');
 
 const should = chai.should();
 
-describe('files-type Unit Tests', function() {
+describe('keywords-type Unit Tests', function() {
+  context('a rule type value should be exported', function() {
+    it('it should equal "standard"', function() {
+      ruleType.should.equal('standard');
+    });
+  });
+
   context('when package.json has node with incorrect type', function() {
     it('LintIssue object should be returned', function() {
       const packageJsonData = {
-        files: 'string'
+        keywords: 'string'
       };
       const response = lint(packageJsonData, 'error');
 
-      response.lintId.should.equal('files-type');
+      response.lintId.should.equal('keywords-type');
       response.lintType.should.equal('error');
-      response.node.should.equal('files');
+      response.node.should.equal('keywords');
       response.lintMessage.should.equal('Type should be an Array');
     });
   });
