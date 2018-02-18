f441a8971ed9819bf7bdd067664526f96b2f643b
tclindner <tclindner@users.noreply.github.com>
2018-02-18 17:14:08 -0600

    Fixed expected type for bundled dependencies (#72)
    Fixes #71

--- src/rules/bundledDependencies-type.js
+++ src/rules/bundledDependencies-type.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const isObject = require('./../validators/type').isObject;
+const isArray = require('./../validators/type').isArray;
 const LintIssue = require('./../LintIssue');
 const lintId = 'bundledDependencies-type';
 const nodeName = 'bundledDependencies';
-const message = 'Type should be an Object';
+const message = 'Type should be an array';
 const ruleType = 'standard';
 
 const lint = function(packageJsonData, lintType) {
-  if (!isObject(packageJsonData, nodeName)) {
+  if (!isArray(packageJsonData, nodeName)) {
     return new LintIssue(lintId, lintType, nodeName, message);
   }
 
