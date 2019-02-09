7bc9545fa18ac390f85941cc5892b8e9f7e0ab7b
tclindner <tclindner@users.noreply.github.com>
2019-02-09 14:34:53 -0600

    Clear console mocks

--- test/unit/Reporter.test.js
+++ test/unit/Reporter.test.js
@@ -23,10 +23,12 @@ describe('Reporter Unit Tests', function() {
         };
         const expectedCallCount = 0;
 
-        jest.spyOn(console, 'log');
+        const consoleMock = jest.spyOn(console, 'log');
 
         Reporter.write(results, false);
         expect(console.log).toHaveBeenCalledTimes(expectedCallCount);
+
+        consoleMock.mockRestore();
       });
 
       test('and one error, zero warning exist, and quiet is false. Spy should be 5', function() {
@@ -51,7 +53,7 @@ describe('Reporter Unit Tests', function() {
         };
         const expectedCallCount = 5;
 
-        jest.spyOn(console, 'log');
+        const consoleMock = jest.spyOn(console, 'log');
 
         Reporter.write(results, false);
         expect(console.log).toHaveBeenCalledTimes(expectedCallCount);
@@ -59,6 +61,8 @@ describe('Reporter Unit Tests', function() {
         expect(console.log).toHaveBeenNthCalledWith(2, chalk.underline('dummyText'));
         expect(console.log).toHaveBeenNthCalledWith(4, chalk.red.bold('1 error'));
         expect(console.log).toHaveBeenNthCalledWith(5, chalk.yellow.bold('0 warnings'));
+
+        consoleMock.mockRestore();
       });
 
       test('and one error, one warning exist, and quiet is false. Spy should be 6', function() {
@@ -89,14 +93,16 @@ describe('Reporter Unit Tests', function() {
         };
         const expectedCallCount = 6;
 
-        jest.spyOn(console, 'log');
+        const consoleMock = jest.spyOn(console, 'log');
 
         Reporter.write(results, false);
         expect(console.log).toHaveBeenCalledTimes(expectedCallCount);
         expect(console.log).toHaveBeenNthCalledWith(1, '');
         expect(console.log).toHaveBeenNthCalledWith(2, chalk.underline('dummyText'));
-        expect(console.log).toHaveBeenNthCalledWith(6, chalk.red.bold('1 error'));
-        expect(console.log).toHaveBeenNthCalledWith(7, chalk.yellow.bold('1 warning'));
+        expect(console.log).toHaveBeenNthCalledWith(5, chalk.red.bold('1 error'));
+        expect(console.log).toHaveBeenNthCalledWith(6, chalk.yellow.bold('1 warning'));
+
+        consoleMock.mockRestore();
       });
 
       test('and one error, one warning exist (filtered out), and quiet is true. Spy should be 4', function() {
@@ -121,13 +127,15 @@ describe('Reporter Unit Tests', function() {
         };
         const expectedCallCount = 4;
 
-        jest.spyOn(console, 'log');
+        const consoleMock = jest.spyOn(console, 'log');
 
         Reporter.write(results, true);
         expect(console.log).toHaveBeenCalledTimes(expectedCallCount);
         expect(console.log).toHaveBeenNthCalledWith(1, '');
         expect(console.log).toHaveBeenNthCalledWith(2, chalk.underline('dummyText'));
         expect(console.log).toHaveBeenNthCalledWith(4, chalk.red.bold('1 error'));
+
+        consoleMock.mockRestore();
       });
 
       test('and two errors, two warnings exist, and quiet is false. Spy should be 8', function() {
@@ -170,7 +178,7 @@ describe('Reporter Unit Tests', function() {
         };
         const expectedCallCount = 8;
 
-        jest.spyOn(console, 'log');
+        const consoleMock = jest.spyOn(console, 'log');
 
         Reporter.write(results, false);
         expect(console.log).toHaveBeenCalledTimes(expectedCallCount);
@@ -178,6 +186,8 @@ describe('Reporter Unit Tests', function() {
         expect(console.log).toHaveBeenNthCalledWith(2, chalk.underline('dummyText'));
         expect(console.log).toHaveBeenNthCalledWith(7, chalk.red.bold('2 errors'));
         expect(console.log).toHaveBeenNthCalledWith(8, chalk.yellow.bold('2 warnings'));
+
+        consoleMock.mockRestore();
       });
     });
 
@@ -217,7 +227,7 @@ describe('Reporter Unit Tests', function() {
         };
         const expectedCallCount = 14;
 
-        jest.spyOn(console, 'log');
+        const consoleMock = jest.spyOn(console, 'log');
 
         Reporter.write(results, false);
 
@@ -234,6 +244,8 @@ describe('Reporter Unit Tests', function() {
         expect(console.log).toHaveBeenNthCalledWith(12, chalk.underline('Totals'));
         expect(console.log).toHaveBeenNthCalledWith(13, chalk.red.bold('2 errors'));
         expect(console.log).toHaveBeenNthCalledWith(14, chalk.yellow.bold('0 warnings'));
+
+        consoleMock.mockRestore();
       });
 
       test('and one error in each file, one warning exist (filtered out), and quiet is true. Spy should be 11', function() {
@@ -271,7 +283,7 @@ describe('Reporter Unit Tests', function() {
         };
         const expectedCallCount = 11;
 
-        jest.spyOn(console, 'log');
+        const consoleMock = jest.spyOn(console, 'log');
 
         Reporter.write(results, true);
         expect(console.log).toHaveBeenCalledTimes(expectedCallCount);
@@ -284,6 +296,8 @@ describe('Reporter Unit Tests', function() {
         expect(console.log).toHaveBeenNthCalledWith(9, '');
         expect(console.log).toHaveBeenNthCalledWith(10, chalk.underline('Totals'));
         expect(console.log).toHaveBeenNthCalledWith(11, chalk.red.bold('2 errors'));
+
+        consoleMock.mockRestore();
       });
     });
   });
