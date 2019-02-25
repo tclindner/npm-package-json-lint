50cd4272db5a23c0304b0b9285fc2c13548a44b6
Ned Zimmerman <ned@bight.ca>
2019-02-25 09:20:11 -0400

    Ensure that repository exists

--- test/unit/rules/require-repository-directory.test.js
+++ test/unit/rules/require-repository-directory.test.js
@@ -9,6 +9,18 @@ describe('require-repository-directory Unit Tests', () => {
     });
   });
 
+  describe('when package.json does not have parent node', () => {
+    test('false should be returned', () => {
+      const packageJsonData = {};
+      const response = lint(packageJsonData, 'error');
+
+      expect(response.lintId).toStrictEqual('require-repository-directory');
+      expect(response.severity).toStrictEqual('error');
+      expect(response.node).toStrictEqual('repository');
+      expect(response.lintMessage).toStrictEqual('repository is required');
+    });
+  });
+
   describe('when package.json has node', () => {
     test('true should be returned', () => {
       const packageJsonData = {
