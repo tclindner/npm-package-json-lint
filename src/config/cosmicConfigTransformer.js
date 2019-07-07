7c13081b052b7b3b53f1906d64221abb7cbf296c
tclindner <tclindner@users.noreply.github.com>
2019-07-07 18:52:11 -0500

    Add initial version of transformer

---
+++ src/config/cosmicConfigTransformer.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const applyExtendsIfSpecified = require('./applyExtendsIfSpecified');
+const applyOverrides = require('./applyOverrides');
+
+const transform = (cwd, configBaseDirectory) => {
+  return cosmiconfigResult => {
+    if (!cosmiconfigResult) {
+      return null;
+    }
+
+    const {config, filepath} = cosmiconfigResult;
+
+    const configDir = configBaseDirectory || path.dirname(filepath || '');
+    const npmPackageJsonLintConfig = {...config};
+
+    const configAfterExtends = applyExtendsIfSpecified(npmPackageJsonLintConfig, filepath);
+    const configAfterOverrides = applyOverrides(cwd, filepath, configAfterExtends.rules, configAfterExtends.overrides);
+
+    return configAfterOverrides;
+  };
+};
+
+module.exports = {
+  transform
+};
