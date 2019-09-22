ec71f8f2aa869c1a1772d029c76ed336a578e68b
tclindner <tclindner@users.noreply.github.com>
2019-09-22 12:24:21 -0500

    Create api.test.js

---
+++ test/unit/api.test.js
@@ -0,0 +1,9 @@
+const api = require('../../src/api');
+
+jest.mock('../../src/NpmPackageJsonLint');
+
+describe('api Unit Tests', () => {
+  test('NpmPackageJsonLint should be exported', () => {
+    expect(api).toHaveProperty('NpmPackageJsonLint');
+  });
+});
