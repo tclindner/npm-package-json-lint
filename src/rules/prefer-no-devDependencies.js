ca1387a67180ddcdab51e9b36659f282dd411bc3
Mikael Korpela <mikael@ihminen.org>
2019-10-04 16:36:43 +0300

    Add: prefer-no-devDependencies

---
+++ src/rules/prefer-no-devDependencies.js
@@ -0,0 +1,20 @@
+const {exists} = require('../validators/property');
+const LintIssue = require('./../LintIssue');
+
+const lintId = 'prefer-no-devDependencies';
+const nodeName = 'devDependencies';
+const message = 'devDependencies should not be defined';
+const ruleType = 'optionalObject';
+
+const lint = (packageJsonData, severity) => {
+  if (exists(packageJsonData, nodeName)) {
+    return new LintIssue(lintId, severity, nodeName, message);
+  }
+
+  return true;
+};
+
+module.exports = {
+  lint,
+  ruleType
+};
