12470e89f15c62602e8395d5243c5e0199b95fca
Sazonov Alexey <effrenus@gmail.com>
2019-12-16 01:20:40 +0300

    feat: Add new rule checking for duplicate in properties (#180)

---
+++ src/rules/no-dup-fields.js
@@ -0,0 +1,29 @@
+const Parser = require('../Parser');
+const {findDuplicatePropNames} = require('../validators/property');
+const LintIssue = require('./../LintIssue');
+
+const lintId = 'no-duplicate-fields';
+const nodeName = '';
+const ruleType = 'optionalObject';
+
+const lint = (packageJsonData, severity) => {
+  /**
+   * If we send package json straight to npm-package-json-lint, fallback to empty string.
+   * Because we already lose information about duplicate properties.
+   */
+  const source = packageJsonData[Parser.sourceSymbol] || ''; // eslint-disable-line
+  const dupProps = findDuplicatePropNames(source);
+
+  if (dupProps.length) {
+    const message = `You have duplicate field names: ${dupProps.join(', ')}. Please remove duplicates.`;
+
+    return new LintIssue(lintId, severity, nodeName, message);
+  }
+
+  return true;
+};
+
+module.exports = {
+  lint,
+  ruleType
+};
---
+++ test/unit/rules/no-dup-fields.test.js
@@ -0,0 +1,63 @@
+const ruleModule = require('./../../../src/rules/no-dup-fields');
+const Parser = require('../../../src/Parser');
+
+const {lint, ruleType} = ruleModule;
+
+const parsePackageJson = source => {
+  const json = JSON.parse(source);
+  json[Parser.sourceSymbol] = source; // eslint-disable-line
+
+  return json;
+};
+
+describe('no-dup-fields Unit Tests', () => {
+  describe('a rule type value should be exported', () => {
+    test('it should equal "optionalObject"', () => {
+      expect(ruleType).toStrictEqual('optionalObject');
+    });
+  });
+
+  describe('when package.json has duplicate fields', () => {
+    test('LintIssue object should be returned', () => {
+      const packageJsonData = parsePackageJson(`{
+        "name": "package1",
+        "name": "package2"
+      }`);
+      const response = lint(packageJsonData, 'error', {exceptions: ['grunt-npm-package-json-lint']});
+
+      expect(response.lintId).toStrictEqual('no-duplicate-fields');
+      expect(response.severity).toStrictEqual('error');
+      expect(response.lintMessage).toStrictEqual('You have duplicate field names: name. Please remove duplicates.');
+    });
+  });
+
+  describe('when package.json has nested duplicate fields', () => {
+    test('LintIssue object should be returned', () => {
+      const packageJsonData = parsePackageJson(`{
+        "name": "package",
+        "devDependencies": {
+          "eslint": "6.7.2",
+          "eslint-config-tc": "9.0.0",
+          "eslint": "6.7.2"
+        }
+      }`);
+      const response = lint(packageJsonData, 'error', {exceptions: ['grunt-npm-package-json-lint']});
+
+      expect(response.lintId).toStrictEqual('no-duplicate-fields');
+      expect(response.severity).toStrictEqual('error');
+      expect(response.lintMessage).toStrictEqual('You have duplicate field names: eslint. Please remove duplicates.');
+    });
+  });
+
+  describe('when package.json without duplicates', () => {
+    test('true should be returned', () => {
+      const packageJsonData = parsePackageJson(`{
+        "name": "package1",
+        "version": "0.1.0"
+      }`);
+      const response = lint(packageJsonData, 'error');
+
+      expect(response).toBe(true);
+    });
+  });
+});
