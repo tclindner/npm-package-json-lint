ecba4f24c2acbcf6b3cf5dde255f145db80c4e42
tclindner <tclindner@users.noreply.github.com>
2020-04-11 22:15:17 -0500

    Update prefer-no-devDependencies to standard rule type
    Closes #159

--- src/rules/prefer-no-devDependencies.js
+++ src/rules/prefer-no-devDependencies.js
@@ -4,7 +4,7 @@ const LintIssue = require('./../LintIssue');
 const lintId = 'prefer-no-devDependencies';
 const nodeName = 'devDependencies';
 const message = 'devDependencies should not be defined';
-const ruleType = 'optionalObject';
+const ruleType = 'standard';
 
 const lint = (packageJsonData, severity) => {
   if (exists(packageJsonData, nodeName)) {
