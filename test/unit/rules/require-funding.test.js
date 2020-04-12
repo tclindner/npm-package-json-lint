1a2bda1792ef6a321818682e0cd16005d4b783fd
Stephen Edgar <stephen@netweb.com.au>
2020-04-12 16:00:12 +1000

    Create require-funding.test.js

---
+++ test/unit/rules/require-funding.test.js
@@ -0,0 +1,34 @@
+const ruleModule = require('./../../../src/rules/require-funding');
+
+const {lint, ruleType} = ruleModule;
+
+describe('require-funding Unit Tests', () => {
+  describe('a rule type value should be exported', () => {
+    test('it should equal "standard"', () => {
+      expect(ruleType).toStrictEqual('standard');
+    });
+  });
+
+  describe('when package.json has node', () => {
+    test('true should be returned', () => {
+      const packageJsonData = {
+        funding: 'funding'
+      };
+      const response = lint(packageJsonData, 'error');
+
+      expect(response).toBe(true);
+    });
+  });
+
+  describe('when package.json does not have node', () => {
+    test('LintIssue object should be returned', () => {
+      const packageJsonData = {};
+      const response = lint(packageJsonData, 'error');
+
+      expect(response.lintId).toStrictEqual('require-funding');
+      expect(response.severity).toStrictEqual('error');
+      expect(response.node).toStrictEqual('funding');
+      expect(response.lintMessage).toStrictEqual('funding is required');
+    });
+  });
+});
