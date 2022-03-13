06d4f905864fa7f646b7bd9863d64b25c7370817
tclindner <5178550+tclindner@users.noreply.github.com>
2022-03-12 20:44:13 -0600

    Ignore @typescript-eslint/naming-convention for test files since we have lots of package.json files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,18 @@
 module.exports = {
   extends: ['eslint-config-tc', 'eslint-config-typescript-tc'],
   rules: {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     'no-prototype-builtins': 'off',
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     'unicorn/filename-case': 'off',
   },
+  overrides: [
+    {
+      files: ['test/**/*.test.ts', 'test/fixtures/**'],
+      rules: {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        '@typescript-eslint/naming-convention': 'off',
+      },
+    },
+  ],
 };
